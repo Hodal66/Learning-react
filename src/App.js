2213a29e4001816b5a5737654e5403d8b793e163
Andela Rwanda <mhthodol@gmail.com>
2022-04-09 23:09:52 +0200

    initial commit by creating react count app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
+import './index.css'
+const App =()=>{
+ const [counter,setCounter] = useState(0)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const handleIncrease=()=>{
+   setCounter(counter + 1)
+ }
+ const handleDecrease =()=>{
+   setCounter(counter - 1)
+ }
+  return(
+    <div className="container">
+      <h1>show me</h1>
+      
+      <button className="btn1" onClick={handleIncrease}>increase</button>
+      <h1>{counter}</h1>
+      <button className="btn2" onClick={handleDecrease}>Decrease</button>
+      </div>
+  )
 }
 
+
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+
+// const App = () => {
+// // Counter is a state initialized to 0
+// const [counter, setCounter] = useState(0)
+
+// // Function is called everytime increment button is clicked
+// const handleClick1 = () => {
+// 	// Counter state is incremented
+// 	setCounter(counter + 1)
+// }
+
+// // Function is called everytime decrement button is clicked
+// const handleClick2 = () => {
+// 	// Counter state is decremented
+// 	setCounter(counter - 1)
+// }
+
+// return (
+// 	<div>
+// 	Counter App
+// 	<div>
+// 		{counter}
+// 	</div>
+// 	<div className="buttons">
+// 		<button className="btn1"
+// 		onClick={handleClick1}>Increment</button>
+// 		<button className="btn2"
+// 		onClick={handleClick2}>Decrement</button>
+// 	</div>
+// 	</div>
+// )
+// }
+
+// export default App
